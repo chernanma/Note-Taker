7723a0ee7ad520a26842f8c128d20a37ccaaaf11
Cesar Martinez <chernanma@msn.com>
2020-11-01 22:15:03 -0500

    Creating apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,22 @@
-// DEPENDENCIES
-var path = require("path");
+// LOAD DATA
+
+var notesData = require("../db/db.json");
+
 
 // ROUTING
 
-module.exports = function(app){
+module.exports = function(app) {
+  // API GET Requests
   
-};
+  app.get("/api/notes", function(req, res) {
+    res.json(notesData);
+  });
+
+  app.post("/api/notes", function(req, res) {        
+      notesData.push(req.body);     
+      res.json(req.body)
+    });
+}
+
+  
+
